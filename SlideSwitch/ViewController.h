614c40f451a35eaacbdf6c8d7c91f3403b158e8b
Michael199311 <pangqiyou@qq.com>
2016-08-31 11:21:00 +0800

    Initial Commit

---
+++ SlideSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideSwitch
+//
+//  Created by 庞启友 on 16/8/31.
+//  Copyright © 2016年 Codger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SlideSwitch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SlideSwitch
+//
+//  Created by 庞启友 on 16/8/31.
+//  Copyright © 2016年 Codger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
